aa384eb8aa4daa7285a364858a9087ee25c67e1c
nicholasuribarri <nicholas.uribarri@gmail.com>
2025-09-09 17:57:08 +0200

    Create AVLTree.py
    initial commit

---
+++ AVLTree.py
@@ -0,0 +1,46 @@
+class AVLNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = "?"
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+
+    def right_rotate(self, node):
+        return
+    
+    def left_rotate(self, node):
+        return
+    
+    def get_height(self, node):
+            if node is None:
+                return 0
+            else:
+                return node.height
+
+    def balance_factor(self, node):
+        if node is None:
+            return 0
+        else:
+            return self.get_height(node.left) - self.get_height(node.right)
+        
+    def balance(self, node):
+        if self.balance_factor(node) < -1:
+            if self.balance_factor(node.right) > 0:
+                node.right = self.right_rotate(node.right)
+            return self.left_rotate(node)
+        
+        if self.balance_factor(node) > 1:
+            if self.balance_factor(node.left) < 0:
+                node.left = self.left_rotate(node.left)
+            return self.right_rotate(node)
+        return node
+    
+
+        
+        
+
