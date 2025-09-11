c26cee708d436e3f3809e6c1d092329c1cf05131
Marigusss <miversen.mi@gmail.com>
2025-09-11 17:04:10 +0200

    oppgave2a.py - lagt til input

--- oppgave2a.py
+++ oppgave2a.py
@@ -1,5 +1,5 @@
 
-liste = [0,1,2,3,4,5,6,7,8,9,10]
+#liste = [0,1,2,3,4,5,6,7,8,9,10]
 #sorted_to_balanced(liste)
 
 
@@ -13,4 +13,16 @@ def sorted(sorted_arr, start, end):
     sorted(sorted_arr, start, middle -1)
     sorted(sorted_arr, middle + 1, end)
 
-sorted(liste, 0, len(liste)-1)
+
+#skriv: 
+#python3 oppgave2a.py < sortert_input*
+# sortert_input er en fil med sorterte heltall nedover
+
+read = sys.stdin.read().splitlines() # en liste med 1 element for hver linje
+input = []
+for i in read:
+    input.append(i)
+
+
+#sorted(liste, 0, len(liste)-1)
+sorted(input, 0, len(input)-1)
